4554439b48e5b80566f6af83fcc8c8cef334e57b
mkcs4760 <mk4cf@mail.umsl.edu>
2019-02-22 11:17:54 -0600

    Shared memory altered to be more in line with the project requirements, though not a simulated clock quite yet.

--- oss.c
+++ oss.c
@@ -6,39 +6,48 @@
 #include <sys/shm.h>
 #include <unistd.h> //used for sleep right now
 
-#define SHSIZE 100
+//#define SHSIZE 100
 
 
 int main(int argc, char *argv[]) {
 	
 	int shmid;
 	key_t key;
-	char * shm;
-	char *s;
+	//char * shm;
+	//char *s;
+	int *a, *b;
 	
 	key = 9876;
-	shmid = shmget(key, SHSIZE, IPC_CREAT | 0666); //this is where we create shared memory
+	shmid = shmget(key, 2*sizeof(int*), IPC_CREAT | 0666); //this is where we create shared memory
 	
 	if(shmid < 0) {
-		perror("shmget");
+		perror("shmget oss side");
 		exit(1);
 	}
 	
-	shm = shmat(shmid, NULL, 0); //attach ourselves to that shared memory
+	//shm = shmat(shmid, NULL, 0); //attach ourselves to that shared memory
+
+	a = shmat(shmid, NULL, 0); //attempting to store 2 integers in shared memory
+	b = a + 1;
 	
-	if(shm == (char *) -1) {
+	if((a == (int *) -1) || (b == (int *) -1)) {
 		perror("shmat");
 		exit(1);
 	}
 	
-	memcpy(shm, "Hello World", 11); //write something to shared memory
+	//memcpy(shm, "Hello World", 11); //write something to shared memory
+	*a = 1;
+	*b = 46;
 	
-	s = shm;
-	s += 11; //the pointer is now at the end of the string we put in shared memory
-	*s = 0; //add terminating 0 at end of string
-	
-	while(*shm != '*')
+	//s = shm;
+	//s += 11; //the pointer is now at the end of the string we put in shared memory
+	//*s = 0; //add terminating 0 at end of string
+	printf("Hello from OSS!\n");
+	while(*a != 25) {
+		*a += 1;
+		printf("From oss, a = %d\n", *a);
 		sleep(1);
+	}
 	
 	int ctl_return = shmctl(shmid, IPC_RMID, NULL);
 	if (ctl_return == -1) {
--- user.c
+++ user.c
@@ -4,39 +4,49 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <unistd.h> //used for sleep right now
 
 
-#define SHSIZE 100
+//#define SHSIZE 100
 
 
 int main(int argc, char *argv[]) {
 	
 	int shmid;
 	key_t key;
-	char * shm;
-	char *s;
+	//char * shm;
+	//char *s;
+	int *a, *b;
 	
 	key = 9876;
-	shmid = shmget(key, SHSIZE, 0666);
+	shmid = shmget(key, 2*sizeof(int*), 0666);
 	
 	if(shmid < 0) {
-		perror("shmget");
+		perror("shmget user side");
 		exit(1);
 	}
 	
-	shm = shmat(shmid, NULL, 0); //attach to shared memory
+	//shm = shmat(shmid, NULL, 0); //attach ourselves to that shared memory
+	a = shmat(shmid, NULL, 0); //attempting to store 2 integers in shared memory
+	b = a + 1;
 	
-	if(shm == (char *) -1) {
+	if((*a == -1) || (*b == -1)) {
 		perror("shmat");
 		exit(1);
 	}
+	printf("Hello from user!\n");
+	while(*a != 25) {
+		//printf("%d  %d", *a, *b);
+		printf("From user, a = %d\n", *a);		
+		sleep(1);
+	}
 	
-	for(s = shm; *s != 0; s++) {
+	/*for(s = shm; *s != 0; s++) {
 		printf("%c", *s);
 	}	
 	
 	printf("\n");
-	*shm = '*';
+	*shm = '*';*/
 	
 	return 0;
 }
